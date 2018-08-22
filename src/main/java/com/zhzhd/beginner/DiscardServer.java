a0197615b4d05829289f57a06878523d2acb8e01
zhangzhendong1 <zhangzhendong1@jd.com>
2018-08-22 11:48:23 +0800

    Initial commit

---
+++ src/main/java/com/zhzhd/beginner/DiscardServer.java
@@ -0,0 +1,70 @@
+package com.zhzhd.beginner;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+;
+/**
+ *@author zhangzhendong1
+ *@date 2018/8/22
+ *@package com.zhzhd.beginner
+ *@describe
+ **/
+public class DiscardServer {
+    private int port;
+
+    public DiscardServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws InterruptedException {
+        // (1)
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // (2)
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    // (3)
+                    .channel(NioServerSocketChannel.class)
+                    // (4)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    // (5)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    // (6)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // (7)
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = b.bind(port).sync();
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new DiscardServer(port).run();
+    }
+}
---
+++ src/main/java/com/zhzhd/beginner/DiscardServerHandler.java
@@ -0,0 +1,35 @@
+package com.zhzhd.beginner;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+;
+/**
+ *@author zhangzhendong1
+ *@date 2018/8/22
+ *@package com.zhzhd.beginner
+ *@describe
+ **/
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter{
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
